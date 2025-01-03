4a97c3750f22a070567bf0a096554119aa48dce3
Dev <androidsoloapps@gmail.com>
2025-01-03 00:26:54 -0500

    feat: implement JWT-based login with secure cookie setup

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
@@ -52,7 +53,19 @@ try{
    if(!isPasswordValid) return res.status(401).json({ message: 'Invalid credentials!' });
 
    // GENERATE COOKIE TOKEN AND SEND TO THE USER
-   res.setHeader('Set-Cookie', 'test=' + 'myValue')
+   //    res.setHeader('Set-Cookie', 'test=' + 'myValue').json({ message: 'Success' });
+
+
+    const token = jwt.sign({ id:user.id }, process.env.JWT_SECRET);
+
+    const age = 1000 * 60 * 60 * 24 * 7; // 1 week
+
+    res.cookie('test2', 'myValue2', {
+        httpOnly: true,
+        // secure: true,
+        maxAge: age,
+    }).status(200).json({ message: 'Login Successful' });
+
 }catch(err){
     console.log(err);
     res.status(500).json({ message: 'Failed to login!' });
