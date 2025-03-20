0683f581f46cb20d3377f105e4d57c74ce3bd389
Dev <androidsoloapps@gmail.com>
2025-03-19 22:32:36 -0500

    feat: add user registration functionality and basic server setup
    - Implemented user registration form with validation and error handling.
    - Integrated axios for API requests.
    - Set up Express server with CORS, JSON parsing, and cookie handling.
    - Added authentication and post routes.

--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,15 +1,60 @@
 import "./register.scss";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+
 
 function Register() {
-    return (
-    <div className="register">
-     <div className="formContainer">
-        <form>
-            <h1>Create an Account</h1>
-            
+  const [error, setError] = useState("")
+  
+  const navigate = useNavigate();
+  
 
+  const handleSubmit = async(e) => {
+  e.preventDefault()
+  const formData = new FormData(e.target);
+  const username = formData.get("username")
+  const email = formData.get("email")
+  const password = formData.get("password")
+
+  try {
+
+  const res = await axios.post("http://localhost:8800/api/auth/register",{
+    username, email, password
+})
+
+
+    navigate("/login")
+
+//   console.log(res.data)
+    
+  
+  }catch(err){
+    console.log(err)
+    setError(err.response.data.message)
+  }
+
+  
+//   console.log(username, email, password)
+
+  };   
+
+  return (
+    <div className="register">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Create an Account</h1>
+          <input name="username" type="text" placeholder="Username" />
+          <input name="email" type="email" placeholder="Email" />
+          <input name="password" type="password" placeholder="Password" />
+          <button>Register</button>
+          {error && <span>{error}</span>}
+          <link to="/login">Do you have an account?</link>
         </form>
-     </div>
-     </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
 }
