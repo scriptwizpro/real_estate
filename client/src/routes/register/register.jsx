a3a3f530fa98b6190631e345311cee32a65010ba
Dev <androidsoloapps@gmail.com>
2025-03-22 22:34:24 -0500

    feat: add user registration form with API integration
    - Implemented a `Register` component using React and SCSS.
    - Integrated form submission with Axios to send registration data to the backend.
    - Added state handling for potential errors.
    - Prevented default form submission behavior.

--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,44 +1,37 @@
 import "./register.scss";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
 
 function Register() {
-  const [error, setError] = useState("")
-  
-  const navigate = useNavigate();
-  
-
-  const handleSubmit = async(e) => {
-  e.preventDefault()
-  const formData = new FormData(e.target);
-  const username = formData.get("username");
-  const email = formData.get("email");
-  const password = formData.get("password");
-
-  try {
-
-  const res = await axios.post("http://localhost:8800/api/auth/register",{
-    username, email, password
-})
+  const [err, setError] = useState("");
 
 
-    navigate("/login")
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new formData(e.target);
 
-//   console.log(res.data)
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    // console.log(username, email, password);
     
-  
-  }catch(err){
-    console.log(err)
-    setError(err.response.data.message)
+    try {
+
+    const res = await axios.post("http://localhost:8800/api/auth/register", {
+      username, email, password
+    })
+
+    console.log(res.data)
+
+  } catch(err) {
+    console.log(err);
+    // setError()
   }
 
-  
-//   console.log(username, email, password)
-
-  };   
-
+  };
   return (
     <div className="registerPage">
       <div className="formContainer">
@@ -48,7 +41,6 @@ function Register() {
           <input name="email" type="email" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
           <button>Register</button>
-          {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
