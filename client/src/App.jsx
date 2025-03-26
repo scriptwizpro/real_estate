c5207b212c414e46744dea9b36b24d82ce415e19
Dev <androidsoloapps@gmail.com>
2025-03-25 23:26:39 -0500

    auth: add login and registration API requests using Axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage/singlePage";
 import Register from "./routes/register/register";
+import Login from "./routes/login/login";
 import ProfilePage from "./routes/profilePage/profilePage";
 
 function App() {
--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -2,15 +2,19 @@ import "./register.scss";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
+import apiRequest from "../../lib/apiRequest";
 
 
 function Register() {
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const navigate = useNavigate();  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError("");
+    setIsLoading(true);
     const formData = new FormData(e.target);
 
     const username = formData.get("username");
@@ -21,7 +25,7 @@ function Register() {
     
     try {
 
-    const res = await axios.post("http://localhost:8800/api/auth/register", {
+    const res = await apiRequest.post("/auth/register", {
       username, email, password
     })
 
@@ -31,18 +35,20 @@ function Register() {
   } catch(err) {
     // console.log(err);
     setError(err.response.data.message);
+  } finally {
+    setIsLoading(false);
   }
 
   };
   return (
-    <div className="registerPage">
+    <div className="register">
       <div className="formContainer">
         <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="email" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button>Register</button>
+          <button disabled={isLoading}>Register</button>
           {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
