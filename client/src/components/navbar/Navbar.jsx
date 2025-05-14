67161a1ce2b88f53c25503a50fc495d69cc9dea1
Dev <androidsoloapps@gmail.com>
2025-05-13 22:42:47 -0500

    Add responsive navbar with user auth state

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
-import { Link } from "react-router-dom";
-
+import { useContext, useState } from 'react';
 import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { AuthContext } from '../../context/AuthContext';
 
 function Navbar() {
-  const [open,setOpen] = useState(false)
+  const [open,setOpen] = useState(false);
+
+  const {currentUser} = useContext(AuthContext)
 
   const user = true;
 
@@ -21,11 +23,13 @@ function Navbar() {
         <a href="/">Agents</a>
         </div>        
         <div className="right">
-        {user ? (
+        {currentUser ? (
           <div className='user'>
-            <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""
+            <img 
+            src={currentUser.avatar || "/noavatar.jpg"}
+            alt=""
             />
-          <span>John Doe</span>
+          <span>{currentUser.username}</span>
           <Link to="/profile" className="profile">
           <div className="notification">3</div>
           <span>Profile</span>
